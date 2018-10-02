27b4d48857564352cf47a0cf4431da6b480c0f14
Ronald Harmsen <ronald@nforza.nl>
2018-10-02 14:07:36 +0200

    Fixed version for demo (Techorama)

---
+++ Frontend/Pages/Fines.cshtml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.ServiceFabric.Services.Client;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Frontend.Pages
+{
+    public class Fine
+    {
+        public string LicensePlate { get; set; }
+        public DateTime Registered { get; set; }
+        public double SpeedInMetersPerSecond { get; set; }
+
+        public double Amount { get; set; }
+    }
+
+    public class FinesModel : PageModel
+    {        
+        public List<Fine> Fines { get; private set; }
+        public string Address { get; set; }
+        public async Task OnGet()
+        {
+            Address = "http://localhost:5000";
+            try
+            {
+                var api = new HttpClient();
+                var vehicleJson = await api.GetStringAsync($"{Address}/api/fines");
+
+                Fines = JsonConvert.DeserializeObject<List<Fine>>(vehicleJson);
+            }
+            catch
+            {
+                Fines = new List<Fine>();
+            }
+        }
+
+        private async Task<string> ResolveApiAddress()
+        {
+            //Warning: Very simplistic/optimistic implementation
+            //This demo uses only 1 partition. Other stateful services might have more..
+            ServicePartitionResolver resolver = ServicePartitionResolver.GetDefault();
+            ResolvedServicePartition partition =
+                await resolver.ResolveAsync(
+                    new Uri("fabric:/TrafficServiceFabric/AdministrativeApi"),
+                    new ServicePartitionKey(0), CancellationToken.None);
+
+            ResolvedServiceEndpoint endpoint = partition.GetEndpoint();
+
+            JObject addresses = JObject.Parse(endpoint.Address);
+            string address = (string)addresses["Endpoints"].First();
+            return address;
+        }
+
+    }
+}
--- VehicleActorService/FineActor.cs
+++ VehicleActorService/FineActor.cs
@@ -1,8 +1,15 @@
 ﻿using Microsoft.ServiceFabric.Actors;
 using Microsoft.ServiceFabric.Actors.Runtime;
+using Microsoft.ServiceFabric.Services.Client;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Fabric;
+using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using VehicleActor;
 
@@ -10,7 +17,7 @@ namespace VehicleActorService
 {
     public interface IFineProcessingActor : IActor
     {
-        Task RegisterMaximumSpeedViolation(DateTime timestamp, string licenseplate, double speedInMetersPerSecond); 
+        Task RegisterMaximumSpeedViolation(DateTime timestamp, string licenseplate, double speedInMetersPerSecond);
     }
 
     public class FineActor : Actor, IFineProcessingActor
@@ -23,7 +30,21 @@ namespace VehicleActorService
         public async Task RegisterMaximumSpeedViolation(DateTime timestamp, string licenseplate, double speedInMetersPerSecond)
         {
             ActorEventSource.Current.ActorMessage(this, $"Registering fine for {licenseplate}, registered at {timestamp} for driving {speedInMetersPerSecond * 3.6}km/hr");
-            await Task.FromResult(0);
+
+            //yikes, just to fix the demo :-)
+            var addr = "http://desktop-euqb4qu:5000/api/fines"; 
+            ActorEventSource.Current.ActorMessage(this, $"Sending fine registration to {addr}");
+
+            var api = new HttpClient();
+            await api.PostAsync(addr,
+                new StringContent(
+                    JsonConvert.SerializeObject(new
+                    {
+                        LicensePlate = licenseplate,
+                        Registered = timestamp,
+                        SpeedInMetersPerSecond = speedInMetersPerSecond
+                    }), Encoding.UTF8, "application/json"
+                ));            
         }
     }
 }
